bec37324e4d2caf7521c468361c66f091501e36e
Eric Bollens <ebollens@ucla.edu>
2012-09-21 17:48:18 -0700

    Add Block builder copy of temporary file to final build locations [#78 #88]

--- rake/build/builder.rb
+++ rake/build/builder.rb
@@ -20,10 +20,42 @@ module WebBlocks
       def dir_build_temp
         @config[:build][:dir_tmp]
       end
+      
+      def file_build_temp_css
+        "#{@config[:build][:dir_tmp]}/core.css"
+      end
+      
+      def file_build_temp_css_ie
+        "#{@config[:build][:dir_tmp]}/ie.css"
+      end
+      
+      def file_build_temp_js
+        "#{@config[:build][:dir_tmp]}/core.js"
+      end
+      
+      def file_build_temp_js_ie
+        "#{@config[:build][:dir_tmp]}/ie.js"
+      end
+      
+      def dir_build_temp_js
+        "#{@config[:build][:dir_tmp]}/script"
+      end
+      
+      def dir_build_temp_img
+        "#{@config[:build][:dir_tmp]}/img"
+      end
 
       def dir_build_metadata
         @config[:build][:dir_metadata]
       end
+      
+      #def append_contents_to_file(src, dst)
+      #  puts "cat #{src} >> #{dst}"
+      #  contents = File.read(src)
+      #  File.open(dst, "a") do |handle|
+      #    handle.puts contents
+      #  end
+      #end
       
     end
     
--- rake/util.rb
+++ rake/util.rb
@@ -1,15 +1,44 @@
+require 'pathname'
+
 module WebBlocks
   
   module Util
         
     def self.dir_from_dir_stack *args
-      pwd = Dir.pwd
-      args.each do |arg|
-        Dir.chdir arg
+      dir_from_dir_stack_array args
+    end
+    
+    def self.dir_from_root_through_dir_stack *args
+      args.unshift dir_root
+      dir_from_dir_stack_array args 
+    end
+        
+    def self.dir_from_dir_stack_array args
+      if args.length == 0
+        Pathname.new(Dir.pwd)
+      elsif
+        subdir = args.shift
+        Dir.chdir subdir do
+          dir_from_dir_stack_array args
+        end
       end
-      path = Pathname.new(Dir.pwd)
-      Dir.chdir pwd
-      path
+    end
+    
+    def self.file_from_dir_stack *args
+      filepath = args.pop
+      args.push File.dirname(filepath)
+      "#{dir_from_dir_stack_array args}/#{File.basename filepath}"
+    end
+    
+    def self.file_from_root_through_dir_stack *args
+      filepath = args.pop
+      args.push File.dirname(filepath)
+      args.unshift dir_root
+      "#{dir_from_dir_stack_array args}/#{File.basename filepath}"
+    end
+      
+    def self.dir_root
+      File.dirname(File.dirname(Pathname.new(__FILE__).realpath))
     end
     
   end

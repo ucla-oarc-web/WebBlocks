e9de3275c57d167850abb63e61439e5d30e1f195
Eric Bollens <ebollens@ucla.edu>
2012-09-21 20:20:00 -0700

    Add Respond.js builder [#78]

--- rake/build/package/respond.rb
+++ rake/build/package/respond.rb
@@ -8,8 +8,34 @@ module WebBlocks
     module Package
     
       class Respond < Builder
-      
-        # TODO
+
+        def dir_package
+          
+          generate_dir_package @config[:package][:respond][:dir]
+          
+        end
+        
+        def build
+          
+          puts ".. Managing Respond.js submodule"
+          
+          manage_submodule 'Respond.js', dir_package
+          
+          puts ".. Packaging Respond.js into Core JS file"
+          
+          if @config[:build][:debug]
+            append_contents_to_file "#{dir_package}/respond.src.js", file_build_temp_js_ie
+          elsif
+            append_contents_to_file "#{dir_package}/respond.min.js", file_build_temp_js_ie
+          end
+          
+        end
+        
+        def reset
+          
+          reset_submodule 'Respond.js', dir_package
+          
+        end
       
       end
       

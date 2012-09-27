3ef4efa4e2feed2bceebdd69c02afd1798b7fd84
Eric Bollens <ebollens@ucla.edu>
2012-09-27 15:26:26 -0700

    Add all logger types and file writing

--- rake/logger.rb
+++ rake/logger.rb
@@ -6,25 +6,75 @@ module WebBlocks
     
     def initialize file
       @file = file
-      FileUtils.touch file
+      @file_handle = File.open @file, "w"
       @scope = 0
+      @types = [:system, :task, :failure, :success, :warning, :info, :debug]
+      @type = :system
+      @types_to_print = [:system, :task, :failure, :success, :warning]
+      @types_to_file = [:system, :task, :failure, :success, :warning, :info, :debug]
     end
     
-    def task category, message
-      scope :task, category, "task: #{message}" do
+    def types_to_print arr
+      arr = @types if arr == :all
+      @types_to_print = arr
+    end
+    
+    def types_to_file arr
+      arr = @types if arr == :all
+      @types_to_file = arr
+    end
+    
+    def task category, message = false
+      scope :task, category, message do
+        yield if block_given?
+      end
+    end
+    
+    def failure category, message = false
+      scope :failure, category, message do
+        yield if block_given?
+      end
+      fail "#{message ? message : category}"
+    end
+    
+    def success category, message = false
+      scope :success, category, message do
+        yield if block_given?
+      end
+    end
+    
+    def warning category, message = false
+      scope :warning, category, message do
+        yield if block_given?
+      end
+    end
+    
+    def info message
+      scope :info, message do
+        yield if block_given?
+      end
+    end
+    
+    def debug message
+      scope :debug, message do
         yield if block_given?
       end
     end
     
     def write type, category, message = false
-      puts message ? "#{'  ' * @scope}[#{category}] #{message}" : "#{'  ' * @scope}#{category}"
+      text = message ? "#{'  ' * @scope}[#{category}] #{message}" : "#{'  ' * @scope}#{category}"
+      puts text if @types_to_print.include? type
+      @file_handle.puts text if @types_to_file.include? type
     end
     
     def scope type, category = false, message = false
-      write type, category, message if category
+      prev = @type
+      @type = type if @types.index(type) > @types.index(@type)
+      write @type, category, message if category
       scope_start
       yield if block_given?
       scope_end
+      @type = prev
     end
     
     def scope_start

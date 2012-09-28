a96904bc34b4dab8460b22badc9d4bafc7ecef5c
Eric Bollens <ebollens@ucla.edu>
2012-09-28 11:50:34 -0700

    Copy JS and images for Bootstrap in builder [fixes #99]

--- rake/build/package/bootstrap.rb
+++ rake/build/package/bootstrap.rb
@@ -13,13 +13,34 @@ module WebBlocks
           generate_dir_package @config[:package][:bootstrap][:dir]
         end
         
-        # TODO: Copy Bootstrap images and Javascript into temp build dir
         def build
           
           @log.task "Builder: Bootstrap", "Managing Bootstrap submodule" do
             updated = manage_submodule 'Bootstrap', dir_package
           end
           
+          @log.task "Builder: Bootstrap", "Packaging Bootstrap Javascript into Core JS file" do
+            @config[:package][:bootstrap][:scripts].each do |script|
+              src = "#{dir_package}/js/bootstrap-#{script}.js"
+              @log.info "bootstrap-#{script}.js"
+              if @config[:build][:debug]
+                append_contents_to_file src, file_build_temp_js
+              elsif
+                append_compressed_js_to_file src, file_build_temp_js
+              end
+            end
+          end
+          
+          @log.task "Builder: Bootstrap", "Packaging Bootstrap images into images directory" do
+            base_dir = "#{dir_package}/img"
+            WebBlocks::Util.get_files(base_dir).each do |src|
+              name = src.sub /^#{base_dir}\//, ''
+              @log.debug name
+              dst = "#{dir_build_temp_img}/#{name}"
+              FileUtils.mkdir_p File.dirname(dst)
+              FileUtils.cp src, dst
+            end
+          end
           
         end
         

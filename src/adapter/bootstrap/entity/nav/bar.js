195f17045518055957ca6dfe787e1ffc8e969444
Eric Bollens <ebollens@ucla.edu>
2012-10-25 13:49:35 -0700

    Add responsive dropdown to nav.bar when overflow occurs [#31]

--- src/adapter/bootstrap/entity/nav/bar.js
+++ src/adapter/bootstrap/entity/nav/bar.js
@@ -1,8 +1,68 @@
-$(window).load(function(){
-    var l = ['top','bottom']
-    for(var i in l){
-        var $navbar = $('nav.bar.'+l[i])
-        if($navbar.length > 0)
-            $('body').css('padding-'+l[i],$navbar.outerHeight(true))
+(function(){
+    
+    var reflowFixedNavbars = function(){
+        var l = ['top','bottom']
+        for(var i in l){
+            var $navbar = $('nav.bar.'+l[i])
+            if($navbar.length > 0)
+                $('body').css('padding-'+l[i],$navbar.outerHeight(true))
+        }
     }
-})
+    
+    var addNavbarOverflowDropdown = function(){
+        $('nav.bar').each(function(){
+            var $ul = $(this).find('ul:not(.dropdown)')
+            if($(this).children('.toggle').length == 0){
+                $ul.after('<a target="#" class="toggle auto">'+($(this).hasClass('bottom') ? '&#x25BC;' : '&#x25B2;')+'</a>')
+            }
+            if($ul.length > 0 && $(this).children('ul.dropdown').length == 0){
+                $ul.clone().attr('class','dropdown').appendTo(this);
+            }
+            $(this).find('.toggle').click(function(){
+                
+                var $closest = $(this).closest('nav.bar.overflow'),
+                    active = $closest.hasClass('active'),
+                    bottom = $closest.hasClass('bottom');
+                
+                if(active)
+                    $closest.removeClass('active')
+                else
+                    $closest.addClass('active')
+                
+                if($(this).hasClass('auto')){
+                    if(bottom ? !active : active)
+                        $(this).html('&#x25BC;')
+                    else
+                        $(this).html('&#x25B2;')
+                }
+            })
+        })
+    }
+    
+    var reflowNavbarOverflows = function(){
+        $('nav.bar.overflow').each(function(){
+            $(this).removeClass('overflow')
+        })
+        $('nav.bar').each(function(){
+            var $children = $(this).find('> * , ul:not(.dropdown) > li'),
+                pos = $children.first().offset().top,
+                overflow = false
+            $children.each(function(){
+                if($(this).is(':visible') && $(this).offset().top != pos)
+                    overflow = true
+            })
+
+            if(overflow){
+                $(this).addClass('overflow')
+            }
+        })
+    }
+    
+    $(window).load(reflowFixedNavbars)
+    $(window).load(addNavbarOverflowDropdown)
+    $(window).load(reflowNavbarOverflows);
+    $(window).resize(reflowNavbarOverflows)
+    
+})()
+
+

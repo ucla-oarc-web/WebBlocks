4fd726c15e5cd7003be29c4716f0b256d5dbcb00
Eric Bollens <ebollens@ucla.edu>
2012-11-09 09:47:27 -0800

    Add close overlay functionality [#123]

---
+++ src/adapter/bootstrap/entity/message/close.js
@@ -0,0 +1,12 @@
+$(window).load(function(){
+    
+    if(typeof $.fn.alert != 'function')
+        return;
+    
+    $('.message').each(function(){
+        if($(this).find('.close').length > 0)
+            return;
+        
+        $(this).prepend('<button type="button" class="close" data-dismiss="alert">×</button>')
+    })
+})

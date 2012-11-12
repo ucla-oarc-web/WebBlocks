038c2c73c9e529478c0d3107955794e6687cb7d6
Logan Franken <loganfranken@gmail.com>
2012-11-11 22:52:00 -0800

    Completed basic implementation of the Entity/Nav/List accordion effect (#64)

---
+++ src/adapter/bootstrap/entity/nav/list.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+
+    $('.list.accordion').on('click', 'a', function (event) {
+        event.preventDefault();
+
+        // Toggle slide animation and "active" class for sublist
+        var $target = $(event.target).closest('li');
+        $target.children('ul,ol').slideToggle('fast', function () {
+            $target.toggleClass('active');
+        });
+    });
+
+});

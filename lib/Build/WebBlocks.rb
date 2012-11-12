15f3928a90ba2b0c60cab1d2373d21c6fcf4390c
Eric Bollens <ebollens@ucla.edu>
2012-11-12 12:20:49 -0800

    Update build hooks [#130]

--- lib/Build/WebBlocks.rb
+++ lib/Build/WebBlocks.rb
@@ -1,8 +1,40 @@
+require 'rubygems'
+require 'extensions/kernel'
+require 'systemu'
+require 'fileutils'
+require_relative '../Path.rb'
+
 module WebBlocks
   
   module Build
       
     class WebBlocks
+      
+      include ::WebBlocks::Path::Source
+      include ::WebBlocks::Path::Temporary_Build
+      include ::WebBlocks::Path::Build
+      
+      def preprocess
+        
+        FileUtils.rm_rf tmp_build_dir
+        FileUtils.mkdir_p tmp_build_dir
+        FileUtils.mkdir_p tmp_css_build_dir
+        FileUtils.mkdir_p tmp_img_build_dir
+        FileUtils.mkdir_p tmp_js_build_dir
+        FileUtils.mkdir_p tmp_js_build_script_dir
+        
+        File.open(tmp_css_build_file, "w") {}
+        File.open(tmp_css_build_file_ie, "w") {}
+        File.open(tmp_js_build_file, "w") {}
+        File.open(tmp_js_build_file_ie, "w") {}
+        
+      end
+      
+      def clean
+        
+        FileUtils.rm_rf build_dir
+        
+      end
       
     end
     
--- lib/Rake/task/build.rb
+++ lib/Rake/task/build.rb
@@ -1,5 +1,18 @@
 task :build => [:init] do
   
-  execute "build"
+  # Packages  =>  Set up the Git submodule
+  # WebBlocks =>  Set up the temp directories and create _WebBlocks.scss linker
+  execute "preprocess"
+  
+  # Packages  =>  Compile if submodule does not come precompiled
+  # WebBlocks =>  Execute the Compass/SASS compiler with _WebBlocks.scss linker
+  execute "compile"
+  
+  # Packages  =>  Copy sources into temporary build files/directories
+  # WebBlocks =>  Copy sources into temporary build files/directories
+  execute "assemble"
+  
+  # WebBlocks =>  Copy temporary build files/directories to final build location
+  execute "package"
   
 end

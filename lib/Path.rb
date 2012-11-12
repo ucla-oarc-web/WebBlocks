da06d257870d4f3345fae33590ee6940cea41da6
Eric Bollens <ebollens@ucla.edu>
2012-11-12 12:19:47 -0800

    Add path computation methods in submodules [#130]

--- lib/Path.rb
+++ lib/Path.rb
@@ -4,10 +4,68 @@ module WebBlocks
   
   module Path
     
-    module Sources
+    module Source
       
       def package_dir package
         ::WebBlocks::Path.from_root_to config[:package][:dir], config[:package][package][:dir]
+      end
+      
+    end
+    
+    module Temporary_Build
+      
+      def tmp_build_dir
+        ::WebBlocks::Path.from_root_to config[:build][:dir_tmp]
+      end
+      
+      def tmp_css_build_dir
+        from_tmp_build_dir_to config[:build][:css][:dir]
+      end
+      
+      def tmp_css_build_file
+        from_tmp_build_dir_to config[:build][:css][:dir], config[:build][:css][:name]
+      end
+      
+      def tmp_css_build_file_ie
+        from_tmp_build_dir_to config[:build][:css][:dir], config[:build][:css][:name_ie]
+      end
+      
+      def tmp_img_build_dir
+        from_tmp_build_dir_to config[:build][:img][:dir]
+      end
+      
+      def tmp_js_build_dir
+        from_tmp_build_dir_to config[:build][:js][:dir]
+      end
+      
+      def tmp_js_build_file
+        from_tmp_build_dir_to config[:build][:js][:dir], config[:build][:js][:name]
+      end
+      
+      def tmp_js_build_file_ie
+        from_tmp_build_dir_to config[:build][:js][:dir], config[:build][:js][:name_ie]
+      end
+      
+      def tmp_js_build_script_dir
+        from_tmp_build_dir_to config[:build][:js][:dir], config[:build][:js][:name_script_dir]
+      end
+      
+      def from_tmp_build_dir_to *args
+        args.unshift tmp_build_dir
+        ::WebBlocks::Path.from_arr_to(args)
+      end
+      
+    end
+    
+    module Build
+      
+      def build_dir
+        ::WebBlocks::Path.from_root_to config[:build][:dir]
+      end
+      
+      def from_build_dir_to *args
+        args.unshift build_dir
+        ::WebBlocks::Path.from_arr_to(args)
       end
       
     end

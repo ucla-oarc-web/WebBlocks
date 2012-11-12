c5d0a4b2970ffab7526aee186ba72dd67c9daa3c
Eric Bollens <ebollens@ucla.edu>
2012-11-12 10:59:35 -0800

    Add Package module for use by package builders [#130]

--- lib/Build/Package.rb
+++ lib/Build/Package.rb
@@ -1,19 +1,77 @@
+require 'rubygems'
+require 'extensions/kernel'
+require 'systemu'
+require 'fileutils'
+require_relative '../Path.rb'
+
 module WebBlocks
   
   module Build
     
     module Package
       
-      def init_submodule
-        puts "init"
-      end
+      include ::WebBlocks::Path::Sources
       
-      def update_submodule
-        puts "update"
-      end
-      
-      def reset_submodule
+      def init_submodule name
         
+        stdout = ""
+        
+        log.task "Package", "Initializing submodule #{name}" do
+        
+          status, stdout, stderr = systemu "#{config[:exec][:git]} submodule init #{package_dir name}"
+
+          if stderr.length > 0
+            log.failure "Package: #{name}", "Initialization failed for submodule #{name}"
+          elsif stdout.length > 0
+            log.success "Package: #{name}", "Initialized submodule #{name}"
+          else
+            log.info "Package: #{name}", "Skipped as submodule #{name} is already initialized"
+          end
+        
+        end
+
+        stdout.length > 0
+        
+      end
+      
+      def update_submodule name
+        
+        stdout = ""
+        
+        log.task "Package", "Updating submodule #{name}" do
+        
+          status, stdout, stderr = systemu "#{config[:exec][:git]} submodule update #{package_dir name}"
+
+          if stderr.length > 0
+            log.failure "Package: #{name}", "Update failed for submodule #{name}"
+          elsif stdout.length > 0
+            log.success "Package: #{name}", "Updated submodule #{name}"
+          else
+            log.info "Package: #{name}", "Skipped as submodule #{name} is already up to date"
+          end
+        
+        end
+
+        stdout.length > 0
+        
+      end
+      
+      def reset_submodule name
+        
+        if File.exists? "#{package_dir name}"
+          
+          log.task "Package", "Removing checkout of #{name}" do
+            FileUtils.rm_rf package_dir name
+          end
+          
+          true
+          
+        else
+          
+          false
+          
+        end
+
       end
       
     end

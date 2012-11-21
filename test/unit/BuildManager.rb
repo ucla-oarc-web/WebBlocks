bea0b26103bb4eb724d43c631bff7749d2a0ef20
Eric Bollens <ebollens@ucla.edu>
2012-11-21 13:43:10 -0800

    Add build manager unit tests

---
+++ test/unit/BuildManager.rb
@@ -0,0 +1,83 @@
+require 'rubygems'
+require 'extensions/kernel'
+require 'fileutils'
+require_relative '../../lib/Test.rb'
+require_relative '../../lib/Build/Utilities.rb'
+
+class TestUnitBuildManager < ::Test::Unit::TestCase 
+  
+  include ::WebBlocks::Test::Unit::TestCase
+  
+  include ::WebBlocks::Build::Utilities
+  
+  def setup_test
+    
+    reset_observers
+    assert_equal 0, observers.length
+    
+  end
+  
+  def test_attach_webblocks
+    
+    attach_webblocks
+    
+    assert_kind_of ::WebBlocks::Build::WebBlocks, observers[0]
+    
+  end
+  
+  def test_attach_module
+    
+    attach_module 'package', 'jquery'
+    
+    assert_kind_of ::WebBlocks::Build::Package::Jquery, observers[0]
+    
+  end
+  
+  def test_attach_packages
+    
+    config[:build][:packages] = [:jquery]
+      
+    attach_packages
+    
+    assert_equal config[:build][:packages].length, observers.length
+    assert_kind_of ::WebBlocks::Build::Package::Jquery, observers[0]
+    
+  end
+  
+  def test_attach_packages_all_with_handlers
+    
+    config[:build][:packages] = [:jquery, :modernizr, :respond, :selectivizr, :efx]
+      
+    attach_packages
+    
+    assert_equal config[:build][:packages].length, observers.length
+    
+    idx = 0
+    config[:build][:packages].each do |package|
+      assert_kind_of eval("::WebBlocks::Build::Package::#{package.to_s.capitalize}"), observers[idx]
+      idx += 1
+    end
+    
+  end
+  
+  def test_attach_core
+    
+    attach_core 'adapter'
+    attach_core 'extensions'
+    attach_core 'definitions'
+    
+    assert_kind_of ::WebBlocks::Build::Core::Adapter, observers[0]
+    assert_kind_of ::WebBlocks::Build::Core::Extensions, observers[1]
+    assert_kind_of ::WebBlocks::Build::Core::Definitions, observers[2]
+    
+  end
+  
+  def test_attach_adapter
+    
+    attach_adapter 'bootstrap'
+    
+    assert_kind_of ::WebBlocks::Build::Adapter::Bootstrap, observers[0]
+    
+  end
+  
+end

02d02379ba03bc7ff7da6dbc60e2fb98eabca9c0
Eric Bollens <ebollens@ucla.edu>
2012-12-03 15:02:28 -0800

    Add JS for text-split-XXX and demo [#189]

---
+++ src/core/adapter/extend/base/type/split.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    
+    var linesClass = 'text-split-lines',
+        wordsClass = 'text-split-words',
+        charsClass = 'text-split-chars'
+
+    $('.'+linesClass).each(function(){
+
+        var $this = $(this)
+
+        $this.lettering('lines')
+
+        if($this.hasClass(wordsClass))
+            $this.removeClass(wordsClass).children('span').addClass(wordsClass)
+
+        if($this.hasClass(charsClass))
+            $this.removeClass(charsClass).children('span').addClass(charsClass)
+
+    })
+
+    $('.'+wordsClass).each(function(){
+
+        var $this = $(this)
+
+        $this.lettering('words')
+
+        if($this.hasClass(charsClass))
+            $this.removeClass(charsClass).children('span').addClass(charsClass)
+
+    })
+
+    $('.'+charsClass).lettering()
+
+});

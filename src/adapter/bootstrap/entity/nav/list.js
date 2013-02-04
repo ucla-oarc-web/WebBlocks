70111fe4ac8d5f0c4f2ad9fd6cca769a9e5ad16d
Logan Franken <loganfranken@gmail.com>
2013-02-04 01:18:17 -0800

    Completed basic implementation of "flyout" effect for the nav/list entity

--- src/adapter/bootstrap/entity/nav/list.js
+++ src/adapter/bootstrap/entity/nav/list.js
@@ -10,4 +10,39 @@ $(function () {
         });
     });
 
+    $('.list.flyout')
+
+    .on('mouseenter', 'li', function (event) {
+
+        var $target = $(event.target);
+
+        // Calculate child list position
+        var position = $target.position();
+
+        var x = Math.ceil(position.left);
+        var width = $target.outerWidth();
+
+        var y = Math.ceil(position.top);
+
+        var paddingRight = $target.css('padding-right');
+        paddingRight = parseInt(paddingRight.replace('px', ''), 8);
+
+        // Base the width of the child list on the root list
+        var $root = $target.closest('.flyout').children('ul,ol');
+        var rootWidth = $root.outerWidth();
+
+        // Display child list
+        $target.siblings('ul,ol').css({
+            'position': 'absolute',
+            'left': x + width - paddingRight,
+            'top': y,
+            'width': rootWidth
+        }).fadeIn();
+
+    })
+
+    .on('mouseleave', 'li', function (event) {
+        $(event.target).siblings('ul,ol').fadeOut();
+    });
+
 });

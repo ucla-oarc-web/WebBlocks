5a41dd44055f70e492ab09b366d884b148196f82
Eric Bollens <ebollens@ucla.edu>
2013-02-19 00:41:45 -0800

    Add responsive support for side menu

--- doc/asset/doc/core.js
+++ doc/asset/doc/core.js
@@ -23,8 +23,24 @@ var DOC = {
             if($.inArray(name.split('/')[0], DOC.menu) < 0){
                 $('#content').append('<div class="panel-12">'+page.render(DOC)+'</div>')
             }else{
-                var menu = new EJS({url: 'component/menu/'+name.split('/')[0]+'.ejs'})
-                $('#content').append('<div class="panel-3">'+menu.render(DOC)+'</div>')
+                var menu = new EJS({url: 'component/menu/'+name.split('/')[0]+'.ejs'}),
+                    menuContents = menu.render(DOC),
+                    menuSet = '<div class="hide-small">'
+                                +menuContents
+                                +'</div>'
+                                +'<div class="show-small" style="margin-bottom:10px;">'
+                                    +'<div data-effect="toggle" data-toggle="show slide" data-target=".menu-show"'
+                                        +' class="menu-show text-center"'
+                                        +' style="padding:8px;background-color:#f2f2f2;color:#1042bc;cursor:pointer;border-radius:8px">'
+                                        +'Show Articles'
+                                    +'</div>'
+                                    +'<div data-effect="toggle" data-toggle="slide" class="menu-show">'
+                                        +menuContents
+                                    +'</div>'
+                                +'</div>'
+                            
+                
+                $('#content').append('<div class="panel-3">'+menuSet+'</div>')
                 $('#content').append('<div class="panel-9">'+page.render(DOC)+'</div>')
             }
             

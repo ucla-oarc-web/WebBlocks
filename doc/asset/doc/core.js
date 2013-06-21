2c58f3c80499e2b8e3da5047076379f59afb1488
Eric Bollens <ebollens@ucla.edu>
2013-06-20 21:27:49 -0700

    Fix #385 - Add prefetching for documentation pages

--- doc/asset/doc/core.js
+++ doc/asset/doc/core.js
@@ -6,7 +6,28 @@ var DOC = {
     
     'modules':{},
     
+    'loading': false,
+    
+    'prefetched': [],
+    
+    'prefetch':function(name){
+
+        if(DOC.prefetched.indexOf(name) >= 0)
+            return 0
+        
+        try{
+            (new EJS({url: 'page/'+name+'.ejs'})).render(DOC);
+            DOC.prefetched.push(name)
+            return 1
+        }catch(e){
+            return -1
+        }
+        
+    },
+    
     'go':function(name, fade){
+
+        DOC.loading = true
 
         var page,
             fadeTime = typeof fade == 'undefined' ? 400 : parseInt(fade);
@@ -51,7 +72,49 @@ var DOC = {
             prettyPrint()
 
             $('#content').fadeIn(fadeTime)
+            
+            DOC.loading = false
+            
+            var currentUrl = window.location.href,
+                hashPos = currentUrl.indexOf('#'),
+                loading = false,
+                prefetchUrl, prefetchAnchor, status
+            if(hashPos >= 0)
+                currentUrl = currentUrl.substring(0,hashPos)
+            
+            $('body a').each(function(){
+                
+                if(DOC.loading)
+                    loading = true // prepare to aboart because new DOC.go() request has been made
+                
+                if(loading)
+                    return false; // abort because a new loading request occurred
+                
+                prefetchUrl = $(this).attr('href')
+                
+                if(typeof prefetchUrl != 'string')
+                    return; // abort because no href
+                
+                if(prefetchUrl.substring(0,hashPos) == currentUrl){
+                    prefetchAnchor = prefetchUrl.substring(hashPos)
+                }else if(prefetchUrl.substring(0,1) == '#'){
+                    prefetchAnchor = prefetchUrl.substring(1)
+                }else{
+                    return; // abort because not an anchor prefetch
+                }
+                
+                status = DOC.prefetch(prefetchAnchor)
+                
+                if(window.console)
+                    switch(status){
+                        case 1: console.log('Prefetch PASS: '+prefetchAnchor); break;
+                        case 0: console.log('Prefetch SKIP: '+prefetchAnchor); break;
+                        default: console.log('Prefetch FAIL: '+prefetchAnchor); break;
+                    }
+                
+            })
 
         })
+        
     }
 }

f216d14cd2ef951e7690a3be5caeef7c6ee42eef
Eric Bollens <ebollens@ucla.edu>
2013-06-20 22:26:37 -0700

    #385 - Remove prefetch because EJS is blocking

--- doc/asset/doc/core.js
+++ doc/asset/doc/core.js
@@ -6,71 +6,7 @@ var DOC = {
     
     'modules':{},
     
-    'loading': false,
-    
-    'prefetched': [],
-    
-    'prefetch':function(name){
-
-        if(DOC.prefetched.indexOf(name) >= 0)
-            return 0
-        
-        try{
-            (new EJS({url: 'page/'+name+'.ejs'})).render(DOC);
-            DOC.prefetched.push(name)
-            return 1
-        }catch(e){
-            return -1
-        }
-        
-    },
-            
-    'prefetchAll': function(){
-
-        var currentUrl = window.location.href,
-            hashPos = currentUrl.indexOf('#'),
-            loading = false,
-            prefetchUrl, prefetchAnchor, status
-    
-        if(hashPos >= 0)
-            currentUrl = currentUrl.substring(0,hashPos)
-
-        $('body a').each(function(){
-
-            if(DOC.loading)
-                loading = true // prepare to aboart because new DOC.go() request has been made
-
-            if(loading)
-                return false; // abort because a new loading request occurred
-
-            prefetchUrl = $(this).attr('href')
-
-            if(typeof prefetchUrl != 'string')
-                return; // abort because no href
-
-            if(prefetchUrl.substring(0,hashPos) == currentUrl){
-                prefetchAnchor = prefetchUrl.substring(hashPos).toLowerCase()
-            }else if(prefetchUrl.substring(0,1) == '#'){
-                prefetchAnchor = prefetchUrl.substring(1).toLowerCase()
-            }else{
-                return; // abort because not an anchor prefetch
-            }
-
-            status = DOC.prefetch(prefetchAnchor)
-
-            if(window.console)
-                switch(status){
-                    case 1: console.log('Prefetch PASS: '+prefetchAnchor); break;
-                    case 0: console.log('Prefetch SKIP: '+prefetchAnchor); break;
-                    default: console.log('Prefetch FAIL: '+prefetchAnchor); break;
-                }
-
-        })
-    },
-    
-    'go':function(name, fade, callback){
-
-        DOC.loading = true
+    'go':function(name, fade){
 
         var page,
             fadeTime = typeof fade == 'undefined' ? 400 : parseInt(fade);
@@ -114,11 +50,7 @@ var DOC = {
             $('#content').efx()
             prettyPrint()
 
-            $('#content').fadeIn(fadeTime, function(){
-                DOC.loading = false
-                if(callback)
-                    callback()
-            })
+            $('#content').fadeIn(fadeTime)
 
         })
         

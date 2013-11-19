750fe923254277683367ad99e8ce9edb9cce87ac
Eric Bollens <ebollens@ucla.edu>
2013-11-19 12:57:02 -0800

    Fix #491 - Add navbar re-invocable initializer

--- src/core/adapter/entity/nav/bar.js
+++ src/core/adapter/entity/nav/bar.js
@@ -1,68 +1,82 @@
-(function(){
-    
-    var reflowFixedNavbars = function(){
-        var l = ['top','bottom']
-        for(var i in l){
-            var $navbar = $('nav.bar.'+l[i])
-            if($navbar.length > 0)
-                $('body').css('padding-'+l[i],$navbar.outerHeight(true)+10)
-        }
-    }
-    
-    var addNavbarOverflowDropdown = function(){
-        $('nav.bar').each(function(){
-            var $ul = $(this).find('ul:not(.dropdown)')
-            if($(this).children('.toggle').length == 0){
-                $ul.after('<a target="#" class="toggle auto">'+($(this).hasClass('bottom') ? '&#x25B2;' : '&#x25BC;')+'</a>')
-            }
-            if($ul.length > 0 && $(this).children('ul.dropdown').length == 0){
-                $ul.clone().attr('class','dropdown').appendTo(this);
-            }
-            $(this).find('.toggle').click(function(){
-                
-                var $closest = $(this).closest('nav.bar.overflow'),
-                    active = $closest.hasClass('active'),
-                    bottom = $closest.hasClass('bottom');
-                
-                if(active)
-                    $closest.removeClass('active')
-                else
-                    $closest.addClass('active')
-                
-                if($(this).hasClass('auto')){
-                    if(bottom ? !active : active)
-                        $(this).html('&#x25BC;')
-                    else
-                        $(this).html('&#x25B2;')
+$(document).addBlocksMethod('navbar',function(command){
+
+    var commands = {
+
+        'init': function(){
+
+            var $this = $(this)
+            $.each(['top','bottom'], function(){
+                var type = this.toString();
+                if($this.filter(function(){$(this).hasClass(type)}).length > 1)
+                    $('body').css('padding-'+type,$(this).outerHeight(true)+10);
+            });
+
+            $this.each(function(){
+                var $ul = $(this).find('ul:not(.dropdown)')
+                if($(this).children('.toggle').length == 0){
+                    $ul.after('<a target="#" class="toggle auto">'+($(this).hasClass('bottom') ? '&#x25B2;' : '&#x25BC;')+'</a>')
                 }
-            })
-        })
-    }
-    
-    var reflowNavbarOverflows = function(){
-        $('nav.bar.overflow').each(function(){
-            $(this).removeClass('overflow')
-        })
-        $('nav.bar').each(function(){
-            var $children = $(this).find('> * , ul:not(.dropdown) > li'),
-                pos = $children.first().offset().top,
-                overflow = false
-            $children.each(function(){
-                if($(this).is(':visible') && $(this).offset().top != pos)
-                    overflow = true
+                if($ul.length > 0 && $(this).children('ul.dropdown').length == 0){
+                    $ul.clone().attr('class','dropdown').appendTo(this);
+                }
+                $(this).find('.toggle').click(function(){
+
+                    var $closest = $(this).closest('nav.bar.overflow'),
+                        active = $closest.hasClass('active'),
+                        bottom = $closest.hasClass('bottom');
+
+                    if(active)
+                        $closest.removeClass('active')
+                    else
+                        $closest.addClass('active')
+
+                    if($(this).hasClass('auto')){
+                        if(bottom ? !active : active)
+                            $(this).html('&#x25BC;')
+                        else
+                            $(this).html('&#x25B2;')
+                    }
+                })
             })
 
-            if(overflow){
-                $(this).addClass('overflow')
-            }
-        })
+            commands['reflow'].apply(this)
+
+        },
+
+        'reflow': function(){
+
+            $(this).removeClass('overflow');
+
+            $(this).each(function(){
+                var $children = $(this).find('> * , ul:not(.dropdown) > li'),
+                    pos = $children.first().offset().top,
+                    overflow = false
+                $children.each(function(){
+                    if($(this).is(':visible') && $(this).offset().top != pos)
+                        overflow = true
+                })
+
+                if(overflow){
+                    $(this).addClass('overflow')
+                }
+            })
+
+        }
+
     }
-    
-    $(window).load(reflowFixedNavbars)
-    $(window).load(addNavbarOverflowDropdown)
-    $(window).load(reflowNavbarOverflows);
-    $(window).resize(reflowNavbarOverflows)
-    
-})();
+
+    commands[command] && commands[command].call($(this), Array.prototype.slice.call(arguments,1));
+
+});
+
+$(window).load(function(){
+
+    $('nav.bar').blocks().navbar('init');
+
+    $(window).resize(function(){
+        $('nav.bar').blocks().navbar('reflow');
+    });
+
+});
 
 
